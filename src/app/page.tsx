68cd8e3df50d9d8b5be234ebd46bc5d8bcfa1433
mzienert <mzienert@gmail.com>
2024-07-15 22:08:50 -0600

    add simple login form, simple styles, install prettier, install tailwindcss/form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,49 +9,67 @@ export default function Home() {
 
   return (
     <>
-        <div>
-            <div>
-                <nav>
-                    <div>
-                        <h1 className="text-red-700"><a href="/">Bird Eater</a></h1>
-                    </div>
-                </nav>
-            </div>
-            <ul>
-                <li>
-                    <a href="#">
-                        <span>Home</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span>About</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span>Contact</span>
-                    </a>
-                </li>
-            </ul>
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto"
+          />
         </div>
-        <main>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
             <div>
-                <a href="#">Login</a>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-white-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-            <header>
-                <h2>stuff</h2>
-            </header>
+
             <div>
-                <h4>Hello World</h4>
-                <div>
-                    <img src="#" alt="" />
-                    <div>
-                        <span>Bird Chilli</span>
-                    </div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-white-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
                 </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-        </main>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
       </>
 
   )
